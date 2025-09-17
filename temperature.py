84813b620fc229f1c22f2336028aa9a98878fa4c
NymeriaChibi <haruhibonita@gmail.com>
2025-09-17 14:43:31 -0600

    Created def celsius_to_fahrenheit

---
+++ temperature.py
@@ -0,0 +1,19 @@
+def celsius_to_fahrenheit(celsius):
+    try:
+        if not isinstance(celsius, (int, float)):
+            raise ValueError('Value must be a number')
+
+        if celsius < -273.15:
+            raise ValueError('Temperature cannot be below zero')
+
+        fahrenheit= (celsius * 9/5)+32
+        return float(fahrenheit)
+    
+    except ValueError as e:
+        print(f'error: {e}')
+
+print(celsius_to_fahrenheit(0))
+print(celsius_to_fahrenheit(100))
+print(celsius_to_fahrenheit(-273.15))
+print(celsius_to_fahrenheit(-500))
+print(celsius_to_fahrenheit("gaku"))
