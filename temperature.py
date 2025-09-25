4cac4d18fa1cd61796429de5efcfab61904097a5
NymeriaChibi <haruhibonita@gmail.com>
2025-09-24 21:50:35 -0600

    organized code. values together, test cases at the end. modified y-intercept name modified to FREEZING_OFFSET

--- temperature.py
+++ temperature.py
@@ -1,6 +1,8 @@
 FREEZING_OFFSET = 32
 SCALE_FACTOR = 5/9
 ABSOLUTE_ZERO_FAHRENHEIT = -459.67
+SLOPE = 9/5
+ABSOLUTE_ZERO_CELSIUS= -273.15
 
 def validate_temperature_input(temp):
     if not isinstance(temp, (int, float)):
@@ -16,11 +18,37 @@ def fahrenheit_to_celsius(fahrenheit):
         return celsius
     except (ValueError, TypeError) as e:
         return f"Error: {e}"
+    
+"----------------------------------------------------------------"
+
+def celsius_to_fahrenheit(celsius):
+    try:
+        validated= validation(celsius)
+        return celsius * SLOPE + FREEZING_OFFSET
+
+    except ValueError as e:
+        print(f'error: {e}')
+        return None
+
+def validation(celsius):
+    if not isinstance(celsius, (int, float)):
+        raise ValueError(f'celsius value provided: {celsius} Value must be a number')
+
+    if celsius < ABSOLUTE_ZERO_CELSIUS:
+        raise ValueError('Temperature cannot be below zero')
+    return celsius
 
 # Test cases
-print("\n--- Test Cases ---")
+print("\n--- Test Fahrenheit Cases ---")
 print(f"32°F = {fahrenheit_to_celsius(32)}°C")
 print(f"100°F = {fahrenheit_to_celsius(100)}°C")
 print(f"-459.67°F = {fahrenheit_to_celsius(-459.67)}°C")
 print(f"-500°F = {fahrenheit_to_celsius(-500)}")
 print(f"'gaku'°F = {fahrenheit_to_celsius('gaku')}")
+print("\n--- Test Celsius Cases ---")
+print(celsius_to_fahrenheit(0))
+print(celsius_to_fahrenheit(100))
+print(celsius_to_fahrenheit(-273.15))
+print(celsius_to_fahrenheit(-500))
+print(celsius_to_fahrenheit("gaku"))
+
