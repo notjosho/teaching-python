f3deb12305735b4e269a8e299d4367b03a0ea6df
NymeriaChibi <haruhibonita@gmail.com>
2025-09-24 21:01:28 -0600

    Added def validation along with values to '32' and '-273.15'

--- temperature.py
+++ temperature.py
@@ -1,9 +1,11 @@
-SCOPE = 9/5
+SLOPE = 9/5
+MAGIC_NUMBER= -273.15
+Y_INTERCEPT= 32
 
 def celsius_to_fahrenheit(celsius):
     try:
-        validation(celsius)
-        return celsius * SCOPE + 32
+        validated= validation(celsius)
+        return celsius * SLOPE + Y_INTERCEPT
 
     except ValueError as e:
         print(f'error: {e}')
@@ -13,8 +15,9 @@ def validation(celsius):
     if not isinstance(celsius, (int, float)):
         raise ValueError(f'celsius value provided: {celsius} Value must be a number')
 
-    if celsius < -273.15:
+    if celsius < MAGIC_NUMBER:
         raise ValueError('Temperature cannot be below zero')
+    return celsius
 
 print(celsius_to_fahrenheit(0))
 print(celsius_to_fahrenheit(100))
