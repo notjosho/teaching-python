486b418ae7dd10c62c7c63512a6edb2df6e4855a
NymeriaChibi <haruhibonita@gmail.com>
2025-09-25 21:52:34 -0600

    formatted code

--- temperature.py
+++ temperature.py
@@ -1,10 +1,11 @@
 FREEZING_OFFSET = 32
 SCALE_FACTOR = 5/9
-ABSOLUTE_ZERO_FAHRENHEIT = -459.67
 SLOPE = 9/5
+ABSOLUTE_ZERO_FAHRENHEIT = -459.67
 ABSOLUTE_ZERO_CELSIUS= -273.15
 
-def validate_temperature_input(temp):
+
+def validate_fahrenheit(temp):
     if not isinstance(temp, (int, float)):
         raise TypeError(f"Input '{temp}' must be a number.")
     if temp < ABSOLUTE_ZERO_FAHRENHEIT:
@@ -13,30 +14,29 @@ def validate_temperature_input(temp):
 
 def fahrenheit_to_celsius(fahrenheit):
     try:
-        validate_temperature_input(fahrenheit)
-        celsius = (fahrenheit - FREEZING_OFFSET) * SCALE_FACTOR
+        validated_fahrenheit= validate_fahrenheit(fahrenheit)
+        celsius = (validated_fahrenheit - FREEZING_OFFSET) * SCALE_FACTOR
         return celsius
     except (ValueError, TypeError) as e:
         return f"Error: {e}"
-    
-"----------------------------------------------------------------"
-
+  
 def celsius_to_fahrenheit(celsius):
     try:
-        validated= validation(celsius)
-        return celsius * SLOPE + FREEZING_OFFSET
+        validated_celsius= validation_celsius(celsius)
+        return validated_celsius * SLOPE + FREEZING_OFFSET
 
     except ValueError as e:
         print(f'error: {e}')
         return None
 
-def validation(celsius):
+def validation_celsius(celsius):
     if not isinstance(celsius, (int, float)):
         raise ValueError(f'celsius value provided: {celsius} Value must be a number')
 
     if celsius < ABSOLUTE_ZERO_CELSIUS:
         raise ValueError('Temperature cannot be below zero')
     return celsius
+
 
 # Test cases
 print("\n--- Test Fahrenheit Cases ---")
@@ -45,6 +45,7 @@ print(f"100°F = {fahrenheit_to_celsius(100)}°C")
 print(f"-459.67°F = {fahrenheit_to_celsius(-459.67)}°C")
 print(f"-500°F = {fahrenheit_to_celsius(-500)}")
 print(f"'gaku'°F = {fahrenheit_to_celsius('gaku')}")
+
 print("\n--- Test Celsius Cases ---")
 print(celsius_to_fahrenheit(0))
 print(celsius_to_fahrenheit(100))
