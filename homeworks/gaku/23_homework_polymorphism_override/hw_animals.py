e2cfe014645f041be21f518ccebb90d41c46f11f
gakupoi <ram.alejandro71@gmail.com>
2025-11-01 15:08:42 -0500

    added homework

---
+++ homeworks/gaku/23_homework_polymorphism_override/hw_animals.py
@@ -0,0 +1,52 @@
+from typing import override
+
+
+class Animal:
+    def __init__(self, name, age):
+        self._name = name
+        self._age = age
+
+    def make_sound(self):
+        return f"{self._name} animal sound"
+
+    def eat(self, food):
+        return f"Animal eats {food}"
+
+    def get_description(self):
+        return f"{self._name} is {self._age} years old"
+
+
+class Dog(Animal):
+    @override
+    def make_sound(self):
+        return f"Woof!"
+
+    @override
+    def eat(self, food):
+        return f"Dog eats {food}"
+
+    def fetch(self):
+        return "Dog fetches the ball"
+
+
+class Cat(Animal):
+    @override
+    def make_sound(self):
+        return "Meow!"
+
+    @override
+    def eat(self, food):
+        return f"Cat eats {food}"
+
+
+class Bird(Animal):
+    @override
+    def make_sound(self):
+        return "Tweet!"
+
+    @override
+    def eat(self, food):
+        return f"Bird eats {food}"
+
+    def fly(self):
+        return "Bird is flying"
